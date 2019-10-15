b8561bce7676c642bdb96e6ac1b02ad60fdd3a17
Nick <Bauer_Nicholas@bah.com>
2019-10-15 14:34:53 -0400

    attempted to scrape second page

--- yelp_spider/spiders/posts.py
+++ yelp_spider/spiders/posts.py
@@ -8,6 +8,17 @@ class PostsSpider(scrapy.Spider):
     start_urls = ['http://www.yelp.com/biz/herndon-centennial-golf-course-herndon/']
 
     def parse(self, response):
+
+    	# TODO Second page wont scrape BUT only has one post so idc
+
+    	# first original scrape
+    	#self.scrape(response)
+
+    	url = response.xpath('//*[@id="super-container"]/div/div/div[1]/div[3]/div[2]/div[3]/div/div/div[2]/div/div[3]/a/@href').extract_first()
+    	yield scrapy.Request(url=url, callback=self.parse)
+    	self.scrape(response)
+
+    def scrape(self, response):
     	posts = response.xpath('.//*[@class="review-content"]')
 
         print '\n\n#################### - STARTING SPIDER - ####################\n\n'
@@ -15,9 +26,15 @@ class PostsSpider(scrapy.Spider):
 
         for post in posts:
         	#text = response.xpath('.//*[@class="review-content"]/p/text()').extract()[i]
+        	#prints off <p> until the frist break <br> 
         	text = post.xpath('.//p[@lang="en"]//text()').extract_first()
 
         	print '\nPost #' + str(count)
         	print text + '\n'
         	count += 1
+
+        #next_page_url = response.xpath('//*[@id="super-container"]/div/div/div[1]/div[3]/div[2]/div[3]/div/div/div[2]/div/div[3]/a/@href').extract_first()
+     	#absolute_next_page_url = response.urljoin(next_page_url)
+     	#yield scrapy.Request(absolute_next_page_url)
+
         print '\n\n##################### - ENDING SPIDER - #####################\n\n'
